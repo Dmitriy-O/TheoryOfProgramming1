03a845d1363ae8b52180c419644043897b5a7e71
Dmitriy-O <dimaovsannikov2@gmail.com>
2024-06-11 17:08:05 +0300

    Minor fixes

--- student/src/test/java/org/byovsiannikov/student/StudentEntityApplicationTests.java
+++ student/src/test/java/org/byovsiannikov/student/StudentEntityApplicationTests.java
@@ -3,7 +3,6 @@ package org.byovsiannikov.student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StudentEntityApplicationTests {
 
     @Test
